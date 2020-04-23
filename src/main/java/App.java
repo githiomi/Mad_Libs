122d134d2e9ffd0d9768be90e11e810a0b0806b2
Dhosio <danielgithiomi@gmail.com>
2020-04-23 18:54:16 +0300

    The form can now receive input and display it on the "/story" page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,17 @@ public class App {
             formLayout.put("template", "templates/form.vtl");
             return new ModelAndView(formLayout, layout);
         }, new VelocityTemplateEngine());
+
+        get("/story", (request, response) ->{
+            Map<String, Object> storyShow = new HashMap<String, Object>();
+            storyShow.put("person1", request.queryParams("person1"));
+            storyShow.put("person2", request.queryParams("person2"));
+            storyShow.put("animal", request.queryParams("animal"));
+            storyShow.put("exclamation", request.queryParams("exclamation"));
+            storyShow.put("verb", request.queryParams("verb"));
+            storyShow.put("noun", request.queryParams("noun"));
+            storyShow.put("template", "templates/story.vtl");
+            return new ModelAndView(storyShow, layout);
+        }, new VelocityTemplateEngine());
     }
 }
